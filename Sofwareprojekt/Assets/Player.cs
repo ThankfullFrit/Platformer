aa38a2004e274acdaeb9761c13edac27b3d4b63a
ThankfullFrit <Julius.kollewijn@gmail.com>
2023-03-19 13:11:55 +0100

    Save 2 Animationen Video2 Version 1
    Animationen erstellt aber nicht alles richtig, teil 1

--- Sofwareprojekt/Assets/Player.cs
+++ Sofwareprojekt/Assets/Player.cs
@@ -8,17 +8,48 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     public float jumph = 5;
     private bool isgrounded = false;
+
+    private Animator anim;
+    private Vector3 rotation;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rotation = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
         float richtung = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+
+        if(richtung != 0)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+        if(richtung < 0)
+        {
+            transform.eulerAngles = rotation - new Vector3(0, 180, 0);
+            transform.Translate(Vector2.right * speed * -richtung * Time.deltaTime);
+        }
+        if (richtung > 0)
+        {
+            transform.eulerAngles = rotation;
+            transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+        }
+        if(isgrounded == false)
+        {
+            anim.SetBool("IsJumping", true);
+        }
+        else
+        {
+            anim.SetBool("IsJumping", false);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
         {
