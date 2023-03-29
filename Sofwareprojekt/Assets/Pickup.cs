c35b4554b1dbcf9b91612ea60ee17d3e043a2029
42069187 <128288136+42069187@users.noreply.github.com>
2023-03-30 01:20:18 +0200

    save 12 added items to inv

---
+++ Sofwareprojekt/Assets/Pickup.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Pickup : MonoBehaviour {
+
+  //  public GameObject item;
+
+
+  //  OnTriggerEnter2D(Collider2D collision)
+  //  {
+  //      PlayerController Player = collision.getcomponent<PlayerController>();
+
+
+    //    if(Player != null)           //wenn ein Player das item aufhebt
+    //    {
+    //        Player.inventory.Add(item);
+    //    }
+  //  }
+
+
+// }
