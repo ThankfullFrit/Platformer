4b427619ea3f3f32d721b89b007fef6cca7a59cb
42069187 <128288136+42069187@users.noreply.github.com>
2023-04-01 00:10:44 +0200

    vollkommen fertig

--- Sofwareprojekt/Assets/Attack.cs
+++ Sofwareprojekt/Assets/Attack.cs
@@ -5,35 +5,37 @@ using UnityEngine.UI;
 
 public class Attack : MonoBehaviour
 {
-    public Transform attackPos;
-    public LayerMask enemies;
-    public float attackrange;
-    public int damage;
+    public Transform attackPos; //öffentliche Variable die die Angriffs Position angibt
+    public LayerMask enemies; //Schichtmaske der Gegner
+    public float attackrange; //Angriffsradius wird definiert
+    public int damage; //Schadensvariable
 
-    private Animator anim;
+    private Animator anim; //private Animator-Variable wird definiert um beim Schlagen eine Animation durchzuführen
 
-    // Start is called before the first frame update
+    
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>(); //Animator wird aufgerufen
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //wenn die linke Maustaste gedrückt wird
         {
-            anim.SetBool("IsAttacking", true);
-            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackrange, enemies);
-            for (int i = 0; i < enemiesToDamage.Length; i++)
+            anim.SetBool("IsAttacking", true); //wird "IsAttacking" auf wahr gesetzt und eine Angriffs Animation wird durchgeführt
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackrange, enemies); //Eine Kollisionsabfrage wird durchgeführt
+                                                                                                                 //und alle Objekte die den Tag "Enemy" haben werden als enemiesToDamage gespeichert 
+                                                                                                                
+            for (int i = 0; i < enemiesToDamage.Length; i++) //for schleife um alle getroffenen Feinde zu durchlaufen
             {
-                enemiesToDamage[i].GetComponent<Enemy>().enemyhealth -= damage;
+                enemiesToDamage[i].GetComponent<Enemy>().enemyhealth -= damage; //die Gesundheit der Gegner wird um den Schaden reduziert
             }
         }
     }
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackrange);
+        Gizmos.color = Color.red; //Farbe = rot
+        Gizmos.DrawWireSphere(attackPos.position, attackrange); //Kugel welche den Angriffsradius sowie den Angriffspunkt zeigt
     }
 }
