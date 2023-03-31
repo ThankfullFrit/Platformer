5dda167d9e6399524872b4f748a12ec918c94952
42069187 <128288136+42069187@users.noreply.github.com>
2023-03-31 21:58:46 +0200

    FAST FERTIG?!?!?!

--- Sofwareprojekt/Assets/Player.cs
+++ Sofwareprojekt/Assets/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     public float jumph = 5;
     private bool isgrounded = false;
-
+    private int jumpCount = 0; // Variable zum Zählen der Sprünge
 
     private Animator anim;
     private Vector3 rotation;
@@ -19,8 +19,6 @@ public class Player : MonoBehaviour
 
     public Vector3 respawnPoint;
     public GameObject fallDetector;
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -64,26 +62,25 @@ public class Player : MonoBehaviour
             anim.SetBool("IsJumping", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && (isgrounded || jumpCount < 2)) // Überprüfung, ob Spieler springen kann
         {
+            rb.velocity = Vector2.zero; // Setzt die vertikale Geschwindigkeit auf Null
             rb.AddForce(Vector2.up * jumph, ForceMode2D.Impulse);
             isgrounded = false;
+            jumpCount++; // Inkrementiert die Anzahl der Sprünge
         }
-        kamera.transform.position = new Vector3(transform.position.x,0, -10);
 
+        kamera.transform.position = new Vector3(transform.position.x, 0, -10);
 
         fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
-
-
-
-
-
     }
+
     public void OnCollisionEnter2D(Collision2D Collision)
     {
         if (Collision.gameObject.tag == "ground")
         {
             isgrounded = true;
+            jumpCount = 0; // Setzt die Anzahl der Sprünge zurück, wenn der Spieler den Boden berührt
         }
         if (Collision.gameObject.tag == "Enemy")
         {
@@ -95,9 +92,8 @@ public class Player : MonoBehaviour
         {
             respawnPoint = transform.position;
         }
-
-
     }
+
     public void OnTriggerEnter2D(Collider2D Collision)
     {
         if (Collision.gameObject.tag == "Spike")
